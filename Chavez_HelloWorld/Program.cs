3df536d996c1e37adcf6e61119efdd6ba8edd3a4
chave <chave@LAPTOP-14FQJQI7>
2024-01-10 04:48:38 -0700

    Add project files.

---
+++ Chavez_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Chavez_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+        }
+    }
+}
